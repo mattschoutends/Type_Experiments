8b1ae5b19bd97a9923816c41a40a7d47721f0e04
Matt Schouten <matt.schouten@hidigital.solutions>
2020-11-17 07:19:02 -0600

    More experiments

--- ProductionCode.UnitTests/TypeExperimentTests.cs
+++ ProductionCode.UnitTests/TypeExperimentTests.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using ProductionCode;
 using Xunit;
 
 namespace ProductionCode.UnitTests
 {
-    public class UnitTest1
+    public class TypeExperimentTests
     {
         [Fact]
         public void CanGetTypeOfObject()
@@ -13,6 +14,71 @@ namespace ProductionCode.UnitTests
             var expected = "System.String";
             var actual = TypeExperiments.GetParameterType("hello");
             actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CanGetTypeOfGeneric()
+        {
+            var list = new List<int>();
+            var expected = "System.Collections.Generic.List`1[System.Int32]";
+
+            var actual = TypeExperiments.GetParameterType(list);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CanGetInternalTypeOfGeneric()
+        {
+            var list = new List<int>();
+            var expected = "System.Int32";
+
+            var actual = TypeExperiments.GetInternalParameterType(list);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CanGetTypeOfBasicGenericInContainer()
+        {
+            var expected = "System.String";
+
+            var container = new ContainerClass<string>();
+            container.Data = "hello";
+
+            var actual = TypeExperiments.GetParameterType(container.Data);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CanGetTypeOfNestedGenericInContainer()
+        {
+            var expected = "System.Collections.Generic.List`1[System.String]";
+
+            var container = new ContainerClass<List<string>>();
+            container.Data = new List<string>() { "hi", "there" };
+
+            var actual = TypeExperiments.GetParameterType(container.Data);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CanReturnContainerForGeneric()
+        {
+            string param = "YES";
+            var actual = TypeExperiments.ReturnContainerOfData(param);
+
+            actual.Data.GetType().ToString().Should().Be("System.String");
+        }
+
+        [Fact]
+        public void CanPutTogetherTwoListsOfGenericType()
+        {
+            var first = new List<string>() { "hello" };
+            var second = new List<string>() { "there", "children" };
+
+            var expected = new List<string>() { "hello", "there", "children" };
+
+            var actual = TypeExperiments.CombineGenericEnumerableThings<IEnumerable<string>, string>(first, second);
+            actual.Should().BeEquivalentTo(expected);
         }
     }
 }
--- ProductionCode/TypeExperiments.cs
+++ ProductionCode/TypeExperiments.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace ProductionCode
 {
     public class ContainerClass<T>
     {
-        bool Success { get; set; }
-        T Data { get; set; }
+        public bool Success { get; set; }
+        public T Data { get; set; }
     }
 
     public static class TypeExperiments
@@ -13,6 +17,30 @@ namespace ProductionCode
         public static string GetParameterType(object o)
         {
             return o.GetType().ToString();
+        }
+
+        public static string GetInternalParameterType(object o)
+        {
+            // Assume it's generic
+            return o.GetType().GenericTypeArguments[0].ToString();
+        }
+
+        public static ContainerClass<T> ReturnContainerOfData<T>(T someData)
+        {
+            var ret = new ContainerClass<T>();
+            ret.Success = true;  // Always successful!
+            ret.Data = someData;
+            return ret;
+        }
+
+        public static T CombineGenericEnumerableThings<T, U>(T first, T second) where T : IEnumerable
+        {
+            IEnumerable<U> one = (IEnumerable<U>)first;
+            IEnumerable<U> two = (IEnumerable<U>)second;
+
+            var combined = one.Concat(two);
+
+            return (T)combined;
         }
     }
 }
